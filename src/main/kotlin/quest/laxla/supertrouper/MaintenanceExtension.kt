4f0fec0d17a66c02dfec8b1cc17e3fb5ff4072f2
LaylaMeower <the@laxla.quest>
2023-12-05 22:48:35 +0200

    Improved some things.

--- src/main/kotlin/quest/laxla/supertrouper/MaintenanceExtension.kt
+++ src/main/kotlin/quest/laxla/supertrouper/MaintenanceExtension.kt
@@ -1,27 +1,11 @@
 package quest.laxla.supertrouper
 
-import com.kotlindiscord.kord.extensions.extensions.Extension
-import com.kotlindiscord.kord.extensions.extensions.ephemeralSlashCommand
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import dev.kord.common.entity.Permission
 import dev.kord.common.entity.Snowflake
 
-class AboutExtension : Extension() {
-	override val name: String
-		get() = "about"
-
+class MaintenanceExtension : TrouperExtension() {
 	override suspend fun setup() {
-		ephemeralSlashCommand {
-			name = "about"
-
-			action {
-				respond {
-					//language=Markdown
-					content = "Hey! This is a test command. It's powered by *magic*:sparkles:"
-				}
-			}
-		}
-
 		publicSlashCommand {
 			name = "stop"
 			description = "WARNING: Stops the bot completely."
