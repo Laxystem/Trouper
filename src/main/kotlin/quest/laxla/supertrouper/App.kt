9f2abd6c95a71eda15aee3412c9f95331bcfb212
LaylaMeower <the@laxla.quest>
2023-12-06 16:02:41 +0200

    Re-sorted some things

--- src/main/kotlin/quest/laxla/supertrouper/App.kt
+++ src/main/kotlin/quest/laxla/supertrouper/App.kt
@@ -3,7 +3,9 @@ package quest.laxla.supertrouper
 import com.kotlindiscord.kord.extensions.ExtensibleBot
 import com.kotlindiscord.kord.extensions.utils.env
 import com.kotlindiscord.kord.extensions.utils.envOrNull
+import dev.kord.gateway.PrivilegedIntent
 import kotlinx.coroutines.runBlocking
+import quest.laxla.supertrouper.messaging.PrivateMassagingExtension
 
 private val token = env("TOKEN")
 private val testingServer = envOrNull("TESTING_SERVER")
@@ -16,6 +18,7 @@ fun main() = runBlocking {
 
 		extensions {
 			add(::MaintenanceExtension)
+			@OptIn(PrivilegedIntent::class)
 			add(::PrivateMassagingExtension)
 		}
 	}.start()
