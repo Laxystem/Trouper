991a0ca8eb3a7a839b76a46562886c5928ee13a6
LaylaMeower <the@laxla.quest>
2023-12-13 17:07:55 +0200

    Almost final bot version

--- src/main/kotlin/quest/laxla/supertrouper/MaintenanceExtension.kt
+++ src/main/kotlin/quest/laxla/supertrouper/MaintenanceExtension.kt
@@ -1,9 +1,9 @@
 package quest.laxla.supertrouper
 
 import com.kotlindiscord.kord.extensions.checks.isBotAdmin
-import com.kotlindiscord.kord.extensions.commands.application.slash.publicSubCommand
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import com.kotlindiscord.kord.extensions.extensions.slashCommandCheck
+import dev.kord.common.entity.Snowflake
 
 class MaintenanceExtension : TrouperExtension() {
 	override suspend fun setup() {
@@ -12,18 +12,14 @@ class MaintenanceExtension : TrouperExtension() {
 		}
 
 		publicSlashCommand {
-			name = "maintenance"
-			description = "Maintenance commands for maintainers of the bot"
+			name = "stop"
+			description = "Stops the bot completely"
+			guildId = Snowflake(officialServer)
 
-			publicSubCommand {
-				name = "stop"
-				description = "Stops the bot completely"
-
-				action {
-					//language=Markdown
-					respond { content = "# Invoking Protocol: Emergency Stop" }
-					bot.stop()
-				}
+			action {
+				//language=Markdown
+				respond { content = "# Invoking Protocol: Emergency Stop" }
+				bot.stop()
 			}
 		}
 	}
--- src/main/kotlin/quest/laxla/supertrouper/messaging/PrivateMessaging.kt
+++ src/main/kotlin/quest/laxla/supertrouper/messaging/PrivateMessaging.kt
@@ -1,25 +1,36 @@
 package quest.laxla.supertrouper.messaging
 
-import com.kotlindiscord.kord.extensions.utils.any
 import com.kotlindiscord.kord.extensions.utils.envOrNull
+import dev.kord.common.entity.ALL
 import dev.kord.common.entity.Permission
+import dev.kord.common.entity.Permissions
+import dev.kord.common.entity.Snowflake
 import dev.kord.core.behavior.GuildBehavior
 import dev.kord.core.behavior.UserBehavior
+import dev.kord.core.behavior.channel.MessageChannelBehavior
+import dev.kord.core.behavior.channel.createMessage
+import dev.kord.core.entity.Member
 import dev.kord.core.entity.User
 import dev.kord.core.entity.channel.Category
+import dev.kord.core.entity.channel.MessageChannel
 import dev.kord.core.entity.channel.TextChannel
+import dev.kord.rest.builder.message.allowedMentions
+import dev.kord.rest.builder.message.create.AbstractMessageCreateBuilder
 import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.firstOrNull
 
 const val PrivateMessagesCategoryName = "Private Messages"
+const val InviteSelect = "PM.invite"
+const val PingButton = "PM.ping"
+const val ManagePmButton = "PM.manage"
+const val UserIdCapturingGroup = "userID"
 val memberLimit = envOrNull("AUTOMATIC_CHANNEL_CREATION_MEMBER_LIMIT")?.toInt() ?: 30
-val privateMessageOwnerPermissions = Permission.ViewChannel + Permission.ReadMessageHistory
-val privateMessageBotPermissions =
-	privateMessageOwnerPermissions + Permission.ManageChannels + Permission.SendMessages + Permission.ManageMessages
+val pmMemberPermissions = Permission.ViewChannel + Permission.ReadMessageHistory
+val pmBotPermissions =
+	pmMemberPermissions + Permission.ManageChannels + Permission.SendMessages + Permission.ManageMessages
+val userMentionRegex = "<@(?<$UserIdCapturingGroup>[1-9][0-9]+)>".toRegex()
 
-suspend infix fun TextChannel.isOf(user: UserBehavior) = topic?.contains(user.mention) == true || pinnedMessages.any {
-	it.author?.id == kord.selfId && it.mentionedUserIds.singleOrNull() == user.id
-}
+infix fun TextChannel.isOf(user: UserBehavior) = topic?.contains(user.mention) == true
 
 suspend fun getChannel(category: Category, user: User) = category.channels.filterIsInstance<TextChannel>().firstOrNull {
 	it.categoryId == category.id && it isOf user
@@ -30,3 +41,36 @@ suspend fun getCategory(guild: GuildBehavior) =
 
 val User.isEligible get() = !isBot
 val Category.isUsableForPrivateMessaging get() = name.equals(PrivateMessagesCategoryName, ignoreCase = true)
+
+fun AbstractMessageCreateBuilder.executePingCommand(
+	channel: MessageChannel,
+	pinger: UserBehavior
+) {
+	val owners = channel.owners?.toList()
+
+	if (owners == null) {
+		allowedMentions()
+
+		content = "The owner of this channel is unknown. They need to be mentioned in the channel's topic, " +
+			"Like this: `<@userID>`."
+	} else {
+		allowedMentions {
+			users.addAll(owners.asSequence().map {
+				Snowflake(it.groups[UserIdCapturingGroup]!!.value)
+			})
+		}
+
+		content = "Hey, " + owners.joinToString(separator = " ") {
+			it.value
+		} + ", y'all were pinged by " + pinger.mention + '!'
+	}
+}
+
+suspend fun MessageChannelBehavior.ping(user: UserBehavior) = createMessage {
+	allowedMentions { users.add(user.id) }
+	content = user.mention
+}.delete(reason = "Ghost pinged " + user.mention)
+
+val MessageChannel.owners get() = data.topic.value?.let { userMentionRegex.findAll(it) }
+
+suspend fun Member.getDeniedPermissions() = Permissions.ALL - getPermissions()
